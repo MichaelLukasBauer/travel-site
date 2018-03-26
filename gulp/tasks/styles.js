810e8fef4d5de76acd103c41ddd77359276c135d
Michael Bauer <michael.bauer@hs-heilbronn.de>
2018-03-26 17:41:34 +0200

    added styles

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,17 @@
+const gulp = require("gulp");
+const postCSS = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssvars = require("postcss-simple-vars");
+const nested = require("postcss-nested");
+const cssImport = require("postcss-import");
+const mixins = require("postcss-mixins");
+
+gulp.task("styles", () => {
+  return gulp
+    .src("./app/assets/styles/styles.css")
+    .pipe(postCSS([cssImport, mixins, cssvars, nested, autoprefixer]))
+    .on("error", error => {
+      console.log(error.toString());
+    })
+    .pipe(gulp.dest("./app/temp/styles"));
+});
