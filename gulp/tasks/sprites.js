3ffbe8cab31096033e6e8aa14e37fe021570b179
Michael Bauer <michael.bauer@hs-heilbronn.de>
2018-03-27 15:21:41 +0200

    completed gulp and template scripts for sprites

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,13 +1,14 @@
 const gulp = require("gulp");
-const postCSS = require("gulp-postcss");
+const rename = require("gulp-rename");
 const svgSprite = require("gulp-svg-sprite");
 
 const config = {
   mode: {
     css: {
+      sprite: "sprite.svg",
       render: {
         css: {
-          template: './gulp/templates/sprites.css'
+          template: "./gulp/templates/sprites.css"
         }
       }
     }
@@ -20,3 +21,18 @@ gulp.task("createSprite", () => {
     .pipe(svgSprite(config))
     .pipe(gulp.dest("./app/temp/sprite/"));
 });
+
+gulp.task("copySpriteGraphic", ["createSprite"], () => {
+  return gulp
+    .src("./app/temp/sprite/css/**/*.svg")
+    .pipe(gulp.dest("./app/assets/images/sprites"));
+});
+
+gulp.task("copySpriteCSS", ["createSprite"], () => {
+  return gulp
+    .src("./app/temp/sprite/css/*.css")
+    .pipe(rename("_sprite.css"))
+    .pipe(gulp.dest("./app/assets/styles/modules"));
+});
+
+gulp.task("icons", ["createSprite", "copySpriteGraphic", "copySpriteCSS"]);
