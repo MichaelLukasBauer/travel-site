83b5f50d0ceda3a44558d62a5fefa271f1e5104f
MichaelLukasBauer <michael.bauer@hs-heilbronn.de>
2018-03-29 16:16:36 +0200

    finished sticky header

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -1,11 +1,20 @@
 import $ from "jquery";
+import smoothScroll from "jquery-smooth-scroll";
 import waypoints from "../../../../node_modules/waypoints/lib/noframework.waypoints";
 
 class StickyHeader {
   constructor() {
     this._siteHeader = $(".site-header");
+    this._pageSection = $(".page-section");
+    this._headerLinks = $(".primary-nav a");
     this._triggerElement = $(".large-hero__title");
     this.createHeaderWaypoint(this._siteHeader, this._triggerElement);
+    this.createPageSectionWaypoints(this._pageSection, this._headerLinks);
+    this.addSmoothScrolling();
+  }
+
+  addSmoothScrolling() {
+    this._headerLinks.smoothScroll();
   }
 
   createHeaderWaypoint(siteHeader, triggerElement) {
@@ -18,6 +27,41 @@ class StickyHeader {
           siteHeader.removeClass("site-header--dark");
         }
       }
+    });
+  }
+
+  createPageSectionWaypoints(pageSections, headerLinks) {
+    pageSections.each(function() {
+      const currentPageSection = this;
+      new Waypoint({
+        element: currentPageSection,
+        handler: direction => {
+          if (direction === "down") {
+            const matchingHeaderLink = currentPageSection.getAttribute(
+              "data-matching-link"
+            );
+            headerLinks.removeClass("is-current-link");
+            $(matchingHeaderLink).addClass("is-current-link");
+          } else {
+          }
+        },
+        offset: "18%"
+      });
+
+      new Waypoint({
+        element: currentPageSection,
+        handler: direction => {
+          if (direction === "up") {
+            const matchingHeaderLink = currentPageSection.getAttribute(
+              "data-matching-link"
+            );
+            headerLinks.removeClass("is-current-link");
+            $(matchingHeaderLink).addClass("is-current-link");
+          } else {
+          }
+        },
+        offset: "-40%"
+      });
     });
   }
 }
